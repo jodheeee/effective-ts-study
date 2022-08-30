44d518e2f9a6973fd3d0c74aa8e284a2d9a10b94
Saemi H <sstyle940104@gmail.com>
2022-08-31 01:05:27 +0900

    feat: add all items

--- src/note/epic/firstChapter/Item06.tsx
+++ src/note/epic/firstChapter/Item06.tsx
@@ -5,7 +5,8 @@ function Item06() {
     <div>
       <p>Item06: transpiler to discover the type system</p>
       <div>
-        * what is tuple type? data structure.배열처럼 여러개의 데이터 배열 + 담아둠
+        * what is tuple type? data structure.배열처럼 여러개의 데이터 배열 +
+        담아둠
         <p>추론정보는(inference) 중요하다</p>
       </div>
     </div>
