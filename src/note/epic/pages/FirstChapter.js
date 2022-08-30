821c952087a5bdb77b32992feacbe511bf890bc0
Saemi H <sstyle940104@gmail.com>
2022-08-30 23:23:17 +0900

    feat: add item05

--- src/note/epic/pages/FirstChapter.js
+++ src/note/epic/pages/FirstChapter.js
@@ -3,6 +3,7 @@ import Item01 from "../firstChapter/Item01";
 import Item02 from "../firstChapter/Item02";
 
 import Item03 from "../firstChapter/Item03";
+import Item04 from "../firstChapter/Item04";
 
 function FirstChapter() {
   return (
@@ -10,6 +11,7 @@ function FirstChapter() {
       <Item01 />
       <Item02 />
       <Item03 />
+      <Item04 />
     </>
   );
 }
