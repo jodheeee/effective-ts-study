63a05e4e2ecd8fdf19954a50316d50cacf451dfb
Saemi H <sstyle940104@gmail.com>
2022-09-13 20:18:02 +0900

    feat: add ThirdChapter

--- src/note/epic/index.tsx
+++ src/note/epic/index.tsx
@@ -1,11 +1,14 @@
 // import FirstChapter from "./pages/FirstChapter";
-import SecondChapter from "./pages/SecondChapter";
+import React from 'react';
+import SecondChapter from './pages/SecondChapter';
+import ThirdChapter from './pages/ThirdChapter';
 
 function EpicIndex() {
   return (
     <div>
       {/* <FirstChapter /> */}
-      <SecondChapter />
+      {/* <SecondChapter /> */}
+      <ThirdChapter />
     </div>
   );
 }
--- src/note/epic/pages/SecondChapter.tsx
+++ src/note/epic/pages/SecondChapter.tsx
@@ -1,9 +1,10 @@
-import Item13 from "../secondChapter/Item13";
-import Item14 from "../secondChapter/Item14";
-import Item15 from "../secondChapter/Item15";
-import Item16 from "../secondChapter/Item16";
-import Item17 from "../secondChapter/Item17";
-import Item18 from "../secondChapter/Item18";
+import React from 'react';
+import Item13 from '../secondChapter/Item13';
+import Item14 from '../secondChapter/Item14';
+import Item15 from '../secondChapter/Item15';
+import Item16 from '../secondChapter/Item16';
+import Item17 from '../secondChapter/Item17';
+import Item18 from '../secondChapter/Item18';
 
 function SecondChapter() {
   return (
