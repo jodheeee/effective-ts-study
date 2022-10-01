3929e8a1f9e9ddb1a4c339fc4c5bb48d08ad112b
Saemi H <sstyle940104@gmail.com>
2022-10-01 18:09:16 +0900

    Feat: switched to sixthchapter

--- src/note/epic/index.tsx
+++ src/note/epic/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import FifthChapter from './pages/FifthChapter';
 import FourthChapter from './pages/FourthChapter';
 import SecondChapter from './pages/SecondChapter';
+import SixthChapter from './pages/SixthChapter';
 import ThirdChapter from './pages/ThirdChapter';
 
 function EpicIndex() {
@@ -12,7 +13,8 @@ function EpicIndex() {
       {/* <SecondChapter /> */}
       {/* <ThirdChapter /> */}
       {/* <FourthChapter /> */}
-      <FifthChapter />
+      {/* <FifthChapter /> */}
+      <SixthChapter />
     </div>
   );
 }
