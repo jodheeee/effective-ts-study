f7efbe4f056b566c1892d97a40c86d3aa960384c
Saemi H <sstyle940104@gmail.com>
2022-10-01 18:08:49 +0900

    feat: add sixthChapterPage

---
+++ src/note/epic/pages/SixthChapter.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Item45 from '../sixthChapter/Item45'
+import Item46 from '../sixthChapter/Item46'
+import Item47 from '../sixthChapter/Item47'
+import Item48 from '../sixthChapter/Item48'
+import Item49 from '../sixthChapter/Item49'
+import Item50 from '../sixthChapter/Item50'
+import Item51 from '../sixthChapter/Item51'
+
+
+const SixthChapter = () => {
+  return (
+    <>
+        <Item45 />
+        <Item46 />
+        <Item47 />
+        <Item48 />
+        <Item49 />
+        <Item50 />
+        <Item51 />
+    </>
+  )
+}
+
+export default SixthChapter
