819ce60e7bac76648e7d7e1be615f426663f57b1
Saemi H <sstyle940104@gmail.com>
2022-10-01 18:06:24 +0900

    Feat: add item51

---
+++ src/note/epic/sixthChapter/Item51.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Item51 = () => {
+  return (
+    <div>
+          <h1>Item51: 의존성 분리위해 mirrored type 사용하기 </h1>
+    <p>
+        필요한 부분만 추출해서 작성중인 library에 넣기.
+    </p>
+    <p>
+        구조적 타이핑 사용
+    </p>
+    </div>
+  )
+}
+
+export default Item51
