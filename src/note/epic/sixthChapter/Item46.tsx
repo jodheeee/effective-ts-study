c628d1cb4e1799ba5d280e0035c2f208d57ff747
Saemi H <sstyle940104@gmail.com>
2022-10-01 17:49:47 +0900

    feat: add Item upto 47 in Chpater six

---
+++ src/note/epic/sixthChapter/Item46.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Item46 = () => {
+  return (
+    <div>
+          <h1>Item46: 타입 선언과 관련된 세 가지 버전 이해하기 </h1>
+    <p>
+        세가지 버전: library, @types, TS versions
+    </p>
+    <p>중복 | 버전 문제 있는지 확인</p>
+    </div>
+  )
+}
+
+export default Item46
